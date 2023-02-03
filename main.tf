30421dd6aa857af7a5d97b73f7da03c5d9f40c58
gmanniv00730 <gmanniv00730@gmail.com>
2023-02-03 15:27:23 -0500

    more shuffling

--- main.tf
+++ main.tf
@@ -1,28 +1,23 @@
-# We strongly recommend using the required_providers block to set the
-# Azure Provider source and version being used
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "3.0.0"
     }
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
 
-# Create a resource group
-resource "tftestresourcegroup" "example" {
-  name     = "tfresourcegroupname"
+resource "azurerm_resource_group" "example" {
+  name     = "my-kubernetes-cluster-resource-group"
   location = "East US 2"
 }
 
-# Create a virtual network within the resource group
-resource "tftestvirtualnetwork" "example" {
-  name                = "tfvirtualnetworkname"
+resource "azurerm_virtual_network" "example" {
+  name                = "my-kubernetes-cluster-virtual-network"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/16"]
